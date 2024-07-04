cdb357ae7331b9b20de1ee73b010242a8a5ecb3a
Julius Kirsch <Julius@skirsch.de>
2024-07-05 00:21:05 +0200

    feat: simple next word text generation using tokenizer

--- Tests/TextGeneration/Test_TextGeneration.cs
+++ Tests/TextGeneration/Test_TextGeneration.cs
@@ -5,65 +5,168 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
+using static System.Net.Mime.MediaTypeNames;
+using SixLabors.ImageSharp;
 
 namespace Tests.TextGeneration
 {
     internal class Test_TextGeneration
     {
-        const string datasetPath = "D:\\testnn\\text_generation\\train.txt"; // Path to your text dataset
-        const int SequenceLength = 100; // Length of each input sequence
-        const int VocabularySize = 256; // ASCII characters
+        const string datasetPath = "D:\\testnn\\text_generation\\train.txt";
+        const int WordsLength = 12;
+        const int VocabularySize = 256; //all ASCii characters
+        const int MaxWordIndexBit = 13; //the number of bits for the arrays
 
         public static void Run()
         {
             string text = File.ReadAllText(datasetPath);
             text = text.Replace("\n", "").Replace("\r", "");
-            int textLength = text.Length;
+            var words = text.Split(new char[] { '.', ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
 
-            float[][] inputs = new float[textLength - SequenceLength][];
-            float[][] targets = new float[textLength - SequenceLength][];
+            int textLength = words.Length;
 
-            for (int i = 0; i < textLength - SequenceLength; i++)
+            var tokenIndexTable = Tokenize(words);
+            
+            Console.WriteLine("Number of unique words: " + tokenIndexTable.Count);
+            int inputSize = textLength / WordsLength;
+            int remaining = textLength % WordsLength;
+
+            if (remaining > 0){
+                textLength -= remaining;
+                inputSize -= 1;
+            }
+
+            float[][] inputs = new float[inputSize][];
+            float[][] targets = new float[inputSize][];
+
+            for (int i = 0; i < textLength - WordsLength; i += WordsLength)
             {
-                inputs[i] = new float[SequenceLength];
-                targets[i] = new float[VocabularySize];
+                if (WordsLength + i > textLength - 1)
+                    break;
 
-                for (int j = 0; j < SequenceLength; j++)
+                float[] inputItem = new float[WordsLength * MaxWordIndexBit];
+                float[] targetItem = new float[MaxWordIndexBit];
+
+                for (int j = 0; j < WordsLength; j++)
                 {
-                    inputs[i][j] = text[i + j];
-                    //Console.Write(inputs[i][j]);
-                }
-                targets[i][text[i + SequenceLength]] = 1;
+                    if (!tokenIndexTable.TryGetValue(words[i + j], out int val))
+                        continue;
 
-                //Console.Write(" -> ");
-                //Console.WriteLine((int)text[i + SequenceLength]);
+                    var bin = GetBinary(val);
+                    Array.Copy(bin, 0, inputItem, j * MaxWordIndexBit, MaxWordIndexBit);
+                }
+
+                inputs[i / WordsLength] = inputItem;
+                //Console.WriteLine("Word: " + iword);
+
+                if (tokenIndexTable.TryGetValue(words[i + WordsLength], out int targetVal))
+                {
+                    targetItem = GetBinary(targetVal);
+                }
+
+                targets[i / WordsLength] = targetItem;
             }
 
             // Build the network
             var network = NetworkBuilder.Create()
-                .Stack(new InputLayer(SequenceLength))
-                .Stack(new LSTMLayer(1024))
-                .Stack(new OutputLayer(VocabularySize, ActivationType.Softmax))
-                .Build(true);
+                .Stack(new InputLayer(WordsLength * MaxWordIndexBit))
+                .Stack(new LSTMLayer(512))
+                .Stack(new OutputLayer(MaxWordIndexBit, ActivationType.Sigmoid))
+                .Build(false);
 
-            network.Load("D:\\textgeneration.cool");
+// network.Load("D:\\textgeneration.cool");
 
             // Train the network
-            network.Train(inputs, targets, 1, 0.1f, 1000);
+            network.Train(inputs, targets, 40, 0.1f, 1000);
 
             // Save the trained model
-            //Console.WriteLine("Press enter to Save");
-            //Console.ReadLine();
+            Console.WriteLine("Press enter to Save");
+            Console.ReadLine();
             network.Save("D:\\textgeneration.cool");
 
-            // Generate text
-            Console.WriteLine("Enter seed text:");
-            string seedText = "If I must not, I need not be barren of accusations; he hath faults";
-            string generatedText = GenerateText(network, seedText, 100);
-            Console.WriteLine("Generated Text:");
-            Console.WriteLine(generatedText);
+            Predict(network, tokenIndexTable, "A sick man's appetite", 5);
         }
 
+        private static void Predict(NNModel model, Dictionary<string, int> tokenIndexTable, string seed, int words = 5)
+        {
+            // Example input sequence
+            string[] inputWords = seed.Split(" ");
+
+            // Prepare the input sequence
+            List<float> inputSequence = PrepareInputSequence(inputWords, tokenIndexTable);
+
+            for (int word = 0; word < words; word++)
+            {
+                float[] predictedBinary = model.Predict(inputSequence.ToArray());
+
+                //get the next word from the prediction:
+                var convertRes = BinaryToInt(predictedBinary);
+
+                if(convertRes.number > tokenIndexTable.Count)
+                {
+                    inputSequence.AddRange(new float[] {0,0,0,0,0,0,0,0});
+                    continue;
+                }
+
+                string nextWord = tokenIndexTable.Keys.ElementAt(convertRes.number);
+
+                inputSequence.AddRange(convertRes.binaryArray);
+
+                Console.WriteLine($"Predicted next word: {nextWord}");
+            }
+        }
+
+        private static List<float> PrepareInputSequence(string[] words, Dictionary<string, int> tokenIndexTable)
+        {
+            List<float> inputItem = new List<float>(WordsLength * 8);
+
+            for (int j = 0; j < WordsLength; j++)
+            {
+                if (j >= words.Length)
+                    break;
+
+                if (!tokenIndexTable.TryGetValue(words[j], out int val))
+                    val = 0;
+
+                var bin = GetBinary(val);
+                inputItem.AddRange(bin);
+            }
+
+            return inputItem;
+        }
+
+        private static (int number, float[] binaryArray) BinaryToInt(float[] binaryArray)
+        {
+            //make a real binary value from all values in the array that are greater than 0.5
+            float[] binaryValues = new float[binaryArray.Length];
+            for (int i = 0; i < binaryArray.Length; i++)
+            {
+                binaryValues[i] = binaryArray[i] > 0.7 ? 1 : 0;
+            }
+
+            string binaryString = string.Join("", binaryValues.Select(b => ((int)b).ToString()));
+            return (Convert.ToInt32(binaryString, 2), binaryArray);
+        }
+
+        private static float[] GetBinary(int number)
+        {
+            //todo make this better and faster
+            string binaryString = Convert.ToString(number, 2);
+
+            var newStr = new string('0', MaxWordIndexBit - binaryString.Length) + binaryString;
+
+            float[] binaryArray = new float[newStr.Length];
+
+            for (int i = 0; i < newStr.Length; i++)
+            {
+                binaryArray[i] = float.Parse(newStr[i].ToString());
+            }
+
+            return binaryArray;
+        }
+
+        /*
         private static string GenerateText(NNModel network, string seedText, int length)
         {
             StringBuilder generated = new StringBuilder(seedText);
@@ -92,6 +195,20 @@ namespace Tests.TextGeneration
             }
 
             return generated.ToString();
+        }
+        */
+        private static Dictionary<string, int> Tokenize(string[] words)
+        {
+            Dictionary<string, int> tokenLookupTable = new();
+            tokenLookupTable.Add(" ", 0);
+
+            foreach(var word in words)
+            {
+                if (!tokenLookupTable.ContainsKey(word))
+                    tokenLookupTable.Add(word, tokenLookupTable.Count - 1);
+            }
+
+            return tokenLookupTable;
         }
 
         private static int ArgMax(float[] array)
